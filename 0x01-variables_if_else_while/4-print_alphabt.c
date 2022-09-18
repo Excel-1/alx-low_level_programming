d2b60e81cba907bd78461214f39622acbf4f0242
Obianuju Nzelu <excellinguj@gmail.com>
2022-09-18 07:58:38 -0700

    just me

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,29 +1,22 @@
 #include <stdio.h>
+
 /**
 * main - Prints all single digit numbers of base 10 starting from 0,
-*        only using putchar and without char variables.
-*
+* only using putchar and without char variables.
 * Return: Always 0.
 */
-
 int main(void)
-
 {
+	char ch = 'a';
 
-	int num;
-
-
-
-	for (num = 0; num < 10; num++)
-
-		putchar((num % 10) + '0');
-
-
-
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		{
+		ch++;
+	}
 	putchar('\n');
-
-
-
 	return (0);
-
 }
